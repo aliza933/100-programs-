8f1e7192bbc3411d162fb8bd365e8f7fc8fd36d7
Aliza fatima <af0253428@gmail.com>
2025-02-05 08:55:40 +0500

    Add files via upload

---
+++ 100.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+class Car {
+public: 
+    string brand;  
+    int year;      
+
+    
+    void displayInfo() {
+        cout << "Brand: " << brand << ", Year: " << year << endl;
+    }
+};
+
+int main() {
+    
+    Car myCar;
+
+    
+    myCar.brand = "Toyota";
+    myCar.year = 2022;
+
+    
+    myCar.displayInfo();
+
+    return 0;
+}
